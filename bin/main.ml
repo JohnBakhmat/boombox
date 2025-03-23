120a18252760af1eafd7560a6385e3682708296f
John Bakhmat <johnbakhmat@gmail.com>
2025-03-23 21:42:08 +0300

    get file path from argv

--- bin/main.ml
+++ bin/main.ml
@@ -2,11 +2,16 @@ open Boombox
 
 let ( let* ) x f = Option.bind x f
 
+let get_filepath () =
+  let argv1 = Sys.argv.(1) in
+  let cwd = Sys.getcwd () in
+  let absolute_path = Core.Filename.to_absolute_exn argv1 ~relative_to:cwd in
+  absolute_path
+;;
+
 let main () =
-  let ic =
-    In_channel.open_bin
-      "/home/johnb/Downloads/03. new one - San Holo, Bipolar Sunshine - bb u ok 2021.flac"
-  in
+  let filepath = get_filepath () in
+  let ic = In_channel.open_bin filepath in
   let* metadata = Flac.read_file ic in
   Metadata.pp_metadata metadata;
   Some ()
