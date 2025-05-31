f89c517ebfb6ef6b13605572d94b7e867d2c4938
John Bakhmat <johnbakhmat@gmail.com>
2025-05-31 09:12:36 +0300

    add env

---
+++ src/env.ts
@@ -0,0 +1,31 @@
+import { Context, Data, Effect, Layer } from "effect"
+import { z } from "zod"
+
+
+const schema = z.object({
+	DB_URL: z.string().min(1)
+})
+type EnvSchema = z.infer<typeof schema>;
+
+class EnvError extends Data.TaggedError("EnvError")<{
+	cause?: unknown,
+	message: string
+}> { }
+
+export class Env extends Context.Tag("Env")<Env, {
+
+	readonly getEnv: Effect.Effect<EnvSchema, EnvError>
+}>() { }
+
+
+export const EnvLive = Layer.succeed(
+	Env,
+	{
+		getEnv: Effect.tryPromise({
+			try: () => schema.parseAsync(process.env),
+			catch: (error) => new EnvError({
+				cause: error,
+				message: "Failed to parse environment variables"
+			})
+		})
+	})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { Console, Effect, Layer } from "effect";
+import { Env, EnvLive } from "./env";
+import { BunContext, BunRuntime } from "@effect/platform-bun";
+
+const main = Effect.gen(function* () {
+
+	const env = yield* Env.pipe(Effect.flatMap(x => x.getEnv))
+
+
+	yield* Console.log(env)
+})
+
+
+const layers = Layer.mergeAll(BunContext.layer, EnvLive)
+
+BunRuntime.runMain(main.pipe(Effect.provide(layers)))
+
