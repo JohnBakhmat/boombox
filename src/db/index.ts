2485757760529cd4b062bff739ed3f20e4c3c6f3
John Bakhmat <johnbakhmat@gmail.com>
2025-05-31 10:00:09 +0300

    fix db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,2 +1,26 @@
 import { SqliteClient } from "@effect/sql-sqlite-bun"
+import { SqlClient } from "@effect/sql"
 
+import * as SqliteDrizzle from "@effect/sql-drizzle/Sqlite"
+import { Effect, Layer } from "effect"
+import { Env } from "../env"
+
+
+//const SqlLive = Layer.scoped(SqliteClient.SqliteClient, Effect.gen(function* () {
+
+//const env = yield* Env.pipe(Effect.flatMap(x => x.getEnv));
+
+//return yield* SqliteClient.make({
+//filename: env.DB_URL
+//})
+//}))
+
+
+const SqlLive = SqliteClient.layer({
+	filename: process.env.DB_URL!
+})
+
+const DrizzleLive = SqliteDrizzle.layer.pipe(
+	Layer.provide(SqlLive)
+)
+export const DatabaseLive = Layer.mergeAll(SqlLive, DrizzleLive)
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import { Console, Effect, Layer } from "effect";
 import { Env, EnvLive } from "./env";
 import { BunContext, BunRuntime } from "@effect/platform-bun";
+import { SqliteDrizzle } from "@effect/sql-drizzle/Sqlite";
+import { songTable } from "./db/schema";
+import { DatabaseLive } from "./db";
 
 const main = Effect.gen(function* () {
 
-	const env = yield* Env.pipe(Effect.flatMap(x => x.getEnv))
+	const db = yield* SqliteDrizzle;
 
+	const data = yield* Effect.promise(() => db.select().from(songTable).execute())
 
-	yield* Console.log(env)
+	yield* Console.log(data)
+
 })
 
 
-const layers = Layer.mergeAll(BunContext.layer, EnvLive)
+const layers = Layer.mergeAll(BunContext.layer, EnvLive, DatabaseLive)
 
 BunRuntime.runMain(main.pipe(Effect.provide(layers)))
 
