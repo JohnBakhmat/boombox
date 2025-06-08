fe357c6ffea1a1a69faa787e5079376efd6e9854
John Bakhmat <johnbakhmat@gmail.com>
2025-06-08 23:00:15 +0300

    actually find vorbis comment

--- src/flac.ts
+++ src/flac.ts
@@ -4,7 +4,9 @@ import {
 	Data,
 	Effect,
 	Equal,
+	ParseResult,
 	pipe,
+	Schema,
 	Sink,
 	Stream,
 } from "effect";
@@ -30,7 +32,96 @@ export const readMetadata = (path: string) =>
 				}),
 			);
 		}
+
+		const file = yield* fs.readFile(path);
+
+		function readHeader(file: Uint8Array, offset: number) {
+			return Effect.gen(function* () {
+				const slice = file.slice(offset, offset + 4);
+
+				const header = yield* Schema.decode(FlacHeaderFromUint8Array)({
+					uint8Array: file,
+					offset: offset,
+				});
+
+				yield* Console.dir({
+					header,
+					offset,
+					slice,
+				});
+
+				return header;
+			});
+		}
+
+		let offset = 4;
+		let header = yield* readHeader(file, offset);
+		offset = offset + 4 + header.length;
+
+		while (!header.isLast && header.streamInfo !== VORBIS_STREAMINFO) {
+			header = yield* readHeader(file, offset);
+			offset = offset + 4 + header.length;
+		}
+
+		yield* Console.log({
+			header,
+			offset,
+		});
 	});
+
+const VORBIS_STREAMINFO = 4;
+
+const FlacHeader = Schema.Struct({
+	isLast: Schema.Boolean, // 1 bit
+	streamInfo: Schema.Number, // 7 bits
+	length: Schema.Number, // 3 bytes
+});
+
+const FlacHeaderFromUint8Array = Schema.transformOrFail(
+	Schema.Struct({
+		uint8Array: Schema.Uint8ArrayFromSelf,
+		offset: Schema.Number,
+	}),
+	FlacHeader,
+	{
+		strict: true,
+		decode({ uint8Array, offset }, _, ast) {
+			return Effect.gen(function* () {
+				if (uint8Array.length < 4) {
+					return yield* ParseResult.fail(
+						new ParseResult.Type(ast, uint8Array, "UIntArray is too short"),
+					);
+				}
+
+				const dataView = new DataView(uint8Array.buffer, offset);
+
+				yield* Console.dir({
+					dataView: dataView,
+				});
+
+				const header = {
+					isLast: (dataView.getUint8(0) & 0x80) === 0x80, // 1 bit
+					streamInfo: dataView.getUint8(0) & 0x7f, // 7 bits
+					length: dataView.getUint32(1, false) >> 8, // 3 bytes
+				};
+
+				return yield* ParseResult.succeed(header);
+			});
+		},
+
+		encode(header, _, ast) {
+			const buffer = new ArrayBuffer(4);
+			const dataView = new DataView(buffer);
+			dataView.setUint8(0, header.isLast ? 0x80 : 0x00);
+			dataView.setUint8(1, header.streamInfo);
+			dataView.setUint32(1, header.length, false);
+			return ParseResult.succeed({
+				uint8Array: new Uint8Array(buffer),
+				offset: 0,
+			});
+		},
+	},
+);
 
 export class FlacError extends Data.TaggedError("FlacError")<{
 	message: string;
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import { BunContext, BunRuntime } from "@effect/platform-bun";
 import { SqliteDrizzle } from "@effect/sql-drizzle/Sqlite";
 import { songTable } from "./db/schema";
 import { DatabaseLive } from "./db";
-import { readVorbisComment } from "./flac";
+import { readMetadata } from "./flac";
 
 const main = Effect.gen(function* () {
-	yield* readVorbisComment("./test-data/11 - Tropical Fish.flac");
+	yield* readMetadata("./test-data/11 - Tropical Fish.flac");
 });
 
 const layers = Layer.mergeAll(BunContext.layer, EnvLive, DatabaseLive);
