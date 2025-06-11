ca11640886ee34f56ae0103825e20afbccbb2bfd
John Bakhmat <johnbakhmat@gmail.com>
2025-06-11 21:41:52 +0300

    update required fields

--- src/flac.ts
+++ src/flac.ts
@@ -13,17 +13,19 @@ import {
 import { FileSystem } from "@effect/platform";
 import { Bit, Int32, Uint8 } from "./utils";
 import { uint8Array } from "@effect/platform/HttpServerResponse";
+import { z } from "zod";
 
-export const isFlac = (path: string) =>
-	Effect.gen(function* () {
+export function isFlac(path: string) {
+	return Effect.gen(function* () {
 		const fs = yield* FileSystem.FileSystem;
 		const file = yield* fs.readFile(path);
-		const slice = new TextDecoder().decode(file.slice(0, 4));
+		const slice = file.slice(0, 4).toString();
 		return slice === "fLaC";
 	});
+}
 
-export const readMetadata = (path: string) =>
-	Effect.gen(function* () {
+export function readMetadata(path: string) {
+	return Effect.gen(function* () {
 		const fs = yield* FileSystem.FileSystem;
 
 		const fileIsFlac = yield* isFlac(path);
@@ -52,150 +54,21 @@ export const readMetadata = (path: string) =>
 			offset,
 		});
 
-		yield* readVorbisComment(file, offset, header.length);
-	});
+		const vorbisComment = yield* readVorbisComment(file, offset, header.length);
 
-function readVorbisComment(file: Uint8Array, offset: number, length: number) {
-	return Effect.gen(function* () {
-		const slice = file.slice(offset, offset + length);
-		yield* Console.log(slice);
-
-		const vorbisComment = yield* Schema.decode(VorbisCommentFromUint8Array)({
-			uint8Array: slice,
-			offset,
-			length,
-		});
-
-		return vorbisComment;
+		yield* Console.table(vorbisComment)
 	});
 }
-
-function readHeader(file: Uint8Array, offset: number) {
-	return Effect.gen(function* () {
-		const slice = file.slice(offset, offset + 4);
-
-		const header = yield* Schema.decode(FlacHeaderFromUint8Array)({
-			uint8Array: file,
-			offset: offset,
-		});
-
-		return header;
-	});
-}
-
-const VorbisComment = Schema.Struct({
-	vendorStringLength: Int32,
-	vendorString: Schema.String,
-	numberOfFields: Int32,
-});
-
-const VorbisCommentFromUint8Array = Schema.transformOrFail(
-	Schema.Struct({
-		uint8Array: Schema.Uint8ArrayFromSelf,
-		offset: Schema.Number,
-		length: Schema.Number,
-	}),
-	VorbisComment,
-	{
-		strict: true,
-		decode({ uint8Array, offset: globalOffset, length }, _, ast) {
-			return Effect.gen(function* () {
-				const dv = new DataView(uint8Array.buffer, globalOffset, length);
-
-				let offset = 0;
-
-				const vendorStringLength = dv.getUint32(offset, true);
-				offset += 4;
-
-				const vendorString = uint8Array
-					.slice(offset, offset + vendorStringLength)
-					.toString();
-				offset += vendorStringLength;
-
-				const numberOfFields = dv.getUint32(offset, true);
-				offset += 4;
-
-				yield* Console.log({
-					vendorStringLength,
-					vendorString: vendorString,
-					numberOfFields,
-				});
-
-				const metadata: Partial<Metadata> = {};
-
-				for (let i = 0; i < numberOfFields; i++) {
-					const fieldLength = dv.getUint32(offset, true);
-					offset += 4;
-
-					const fieldValue = uint8Array
-						.slice(offset, offset + fieldLength)
-						.toString();
-					offset += fieldLength;
-
-					const parsedField = parseFieldValue(fieldValue);
-
-					if (!parsedField) {
-						continue;
-					}
-
-					const { key, value } = parsedField;
-
-					yield* Console.log({
-						offset,
-						fieldLength,
-						fieldValue,
-						key,
-						value,
-					});
-
-					switch (key) {
-						case "ALBUM":
-							metadata.album = value;
-							break;
-						case "ARTIST":
-							metadata.artist = value;
-							break;
-						case "TITLE":
-							metadata.title = value;
-							break;
-						default:
-							continue;
-					}
-				}
-
-				yield* Console.log(metadata);
-
-				const vorbisComment = {
-					vendorStringLength,
-					vendorString,
-					numberOfFields,
-				};
-				return yield* ParseResult.succeed(vorbisComment);
-			});
-		},
-		encode(x, _, ast) {
-			const arrayBuffer = new ArrayBuffer(64);
-			const dv = new DataView(arrayBuffer);
-			dv.setInt32(0, x.vendorStringLength);
-
-			return ParseResult.fail(new ParseResult.Type(ast, x, "Unimplemented"));
-		},
-	},
-);
-
-function parseFieldValue(str: string): { key: string; value: string } | null {
-	const [key, value] = str.split("=").map((x) => x.trim());
-	if (!key || !value) return null;
-	return { key, value };
-}
-
-type Metadata = {
-	album: string;
-	artist: string;
-	title: string;
-};
 
 const VORBIS_STREAMINFO = 4;
+
+const metadataSchema = z.object({
+	album: z.string(),
+	artist: z.string(),
+	title: z.string(),
+});
+
+type Metadata = z.infer<typeof metadataSchema>;
 
 const FlacHeader = Schema.Struct({
 	isLast: Bit, // 1 bit
@@ -244,6 +117,145 @@ const FlacHeaderFromUint8Array = Schema.transformOrFail(
 		},
 	},
 );
+
+const VorbisComment = Schema.Struct({
+	artist: Schema.String,
+	album: Schema.String,
+	title: Schema.String,
+});
+
+const VorbisCommentFromUint8Array = Schema.transformOrFail(
+	Schema.Struct({
+		uint8Array: Schema.Uint8ArrayFromSelf,
+		offset: Schema.Number,
+		length: Schema.Number,
+	}),
+	VorbisComment,
+	{
+		strict: true,
+		decode({ uint8Array, offset, length }, _, ast) {
+			return Effect.gen(function* () {
+				const dv = new DataView(uint8Array.buffer, offset, length);
+
+				let cursor = 0;
+
+				const vendorStringLength = dv.getUint32(cursor, true);
+				cursor += 4;
+
+				const vendorString = uint8Array
+					.slice(cursor, cursor + vendorStringLength)
+					.toString();
+				cursor += vendorStringLength;
+
+				const numberOfFields = dv.getUint32(cursor, true);
+				cursor += 4;
+
+				const metadata: Partial<Metadata> = {};
+
+				for (let i = 0; i < numberOfFields; i++) {
+					const fieldLength = dv.getUint32(cursor, true);
+					cursor += 4;
+
+					const fieldValue = uint8Array
+						.slice(cursor, cursor + fieldLength)
+						.toString();
+					cursor += fieldLength;
+
+					const parsedField = parseFieldValue(fieldValue);
+
+					if (!parsedField) {
+						continue;
+					}
+
+					const { key, value } = parsedField;
+
+					switch (key) {
+						case "ALBUM":
+							metadata.album = value;
+							break;
+						case "ARTIST":
+							metadata.artist = value;
+							break;
+						case "TITLE":
+							metadata.title = value;
+							break;
+						default:
+							continue;
+					}
+				}
+
+				//catch:(error)=> ParseResult.fail(new ParseResult.Type(ast, error, "Failed to validate parsed metdata"))
+
+				//const foo = effect.promise(()=>metadataschema.parseasync(metadata))
+
+				//const valid = yield* Effect.tryPromise(() =>
+				//metadataSchema.parseAsync(metadata),
+				//).pipe(
+				//Effect.mapError((error) =>
+				//ParseResult.fail(
+				//new ParseResult.Type(ast, error, "Failed to validate"),
+				//),
+				//),
+				//Effect.map(ParseResult.succeed)
+				//);
+
+				//return yield* valid
+
+				const {
+					data: valid,
+					success: isValid,
+					error,
+				} = yield* Effect.promise(() =>
+					metadataSchema.safeParseAsync(metadata),
+				);
+
+				if (!isValid) {
+					return yield* ParseResult.fail(
+						new ParseResult.Type(ast, error, "Metadata is not valid"),
+					);
+				}
+				return yield* ParseResult.succeed(valid);
+			});
+		},
+		encode(x, _, ast) {
+			return ParseResult.fail(new ParseResult.Type(ast, x, "Unimplemented"));
+		},
+	},
+);
+
+function readVorbisComment(file: Uint8Array, offset: number, length: number) {
+	return Effect.gen(function* () {
+		const slice = file.slice(offset, offset + length);
+		yield* Console.log(slice);
+
+		const vorbisComment = yield* Schema.decode(VorbisCommentFromUint8Array)({
+			uint8Array: slice,
+			offset,
+			length,
+		});
+
+		return vorbisComment;
+	});
+}
+
+function readHeader(file: Uint8Array, offset: number) {
+	return Effect.gen(function* () {
+		const slice = file.slice(offset, offset + 4);
+
+		const header = yield* Schema.decode(FlacHeaderFromUint8Array)({
+			uint8Array: file,
+			offset: offset,
+		});
+
+		return header;
+	});
+}
+
+function parseFieldValue(str: string): { key: string; value: string } | null {
+	const [key, value] = str.split("=").map((x) => x.trim());
+	if (!key || !value) return null;
+	return { key, value };
+}
 
 export class FlacError extends Data.TaggedError("FlacError")<{
 	message: string;
