e56d968024ea57821dc44ae8f753bb4b91575f6c
John Bakhmat <johnbakhmat@gmail.com>
2025-06-13 11:46:46 +0300

    feat: add file parsing for FLAC and implement tests

---
+++ src/file-parser.ts
@@ -0,0 +1,46 @@
+import { Effect, Either, Option } from "effect";
+import * as flac from "./flac";
+
+const SUPPORTED_EXTENSIONS = ["flac"] as const;
+type SupportedExtension = (typeof SUPPORTED_EXTENSIONS)[number];
+
+export function parseFile(path: string) {
+	return Effect.gen(function* () {
+		const extension = path.split(".").pop();
+		const assumedType =
+			extension &&
+			SUPPORTED_EXTENSIONS.includes(extension as SupportedExtension)
+				? (extension as SupportedExtension)
+				: null;
+
+		if (!assumedType) {
+			return yield* Effect.fail(new Error("File is unsupported"));
+		}
+
+		if (assumedType === "flac") {
+			const isFlac = yield* flac.isFlac(path);
+			if (isFlac) {
+				const metadata = yield* flac.readMetadata(path);
+				return metadata;
+			}
+		}
+		return yield* Effect.fail(new Error("File is unsupported"));
+	});
+}
+
+export function parseManyFiles(paths: string[]) {
+	return Effect.gen(function* () {
+		const tasks = paths.map((path) => parseFile(path));
+
+		const files = yield* Effect.all(tasks, {
+			concurrency: 10,
+			mode: "either",
+		});
+
+		const successful = files
+			.filter((file) => Either.isRight(file))
+			.map((file) => file.right);
+
+		return successful;
+	});
+}
---
+++ test/file-parser.test.ts
@@ -0,0 +1,29 @@
+import { expect, it as test } from "@effect/vitest";
+import { Effect } from "effect/index";
+import { BunContext } from "@effect/platform-bun/index";
+import { parseFile, parseManyFiles } from "../src/file-parser";
+import { Console } from "effect";
+
+const provide = Effect.provide(BunContext.layer);
+
+test.effect("parseFile should parse flac just fine", () =>
+	Effect.gen(function* () {
+		const input = "./test-data/11 - Tropical Fish.flac";
+		const actual = yield* parseFile(input);
+		expect(actual).toMatchSnapshot();
+	}).pipe(provide),
+);
+
+test.effect("parseManyFiles should parse many files just fine", () =>
+	Effect.gen(function* () {
+		const input = [
+			"./test-data/11 - Tropical Fish.flac",
+			"./test-data/01 - hover.mp3",
+		];
+		const actual = yield* parseManyFiles(input);
+
+		yield* Console.dir(actual);
+
+		expect(actual.length).toBe(1); // mp3 is not yet supported
+	}).pipe(provide),
+);
