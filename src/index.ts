fa81fd6a00f0a497cc3e15f1978de8cb06861cbb
John Bakhmat <johnbakhmat@gmail.com>
2025-06-22 11:18:54 +0300

    fix deps i guess

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import { Effect, Layer } from "effect";
+import { Console, Effect, Layer } from "effect";
 import { EnvLive } from "./env";
 import { BunContext, BunRuntime } from "@effect/platform-bun";
-
 
 import { DatabaseLive } from "./db";
 import { readMetadata } from "./flac";
 
-const main = Effect.gen(function* () {
-	yield* readMetadata("./test-data/11 - Tropical Fish.flac");
-});
 
 const layers = Layer.mergeAll(BunContext.layer, EnvLive, DatabaseLive);
 
-BunRuntime.runMain(main.pipe(Effect.provide(layers)));
+const main = Effect.gen(function* () {
+    const metadata = yield* readMetadata("./test-data/11 - Tropical Fish.flac");
+
+    yield* Console.log(metadata);
+}).pipe(Effect.provide(layers));
+
+
+BunRuntime.runMain(main);
