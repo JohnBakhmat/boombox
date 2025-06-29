15abbacde20f99fe6636662a5a27eac5dccf415c
John Bakhmat <johnbakhmat@gmail.com>
2025-06-29 12:03:03 +0300

    feat: add int24 uint7 and cleanup some magic numbers

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,24 @@
-import { Schema } from "effect";
+import { Effect, Option, Schema } from "effect";
 
 export const Nibble = Schema.Number.pipe(
 	Schema.between(0, 15, {
 		identifier: "Nibble",
 		description: "a 4-bit unsigned integer",
+	}),
+);
+
+export const Uint7 = Schema.Number.pipe(
+	Schema.between(0, 127, {
+		identifier: "Uint7",
+		description: "a 7-bit unsigned integer",
+	}),
+);
+
+const INT24_MAX = 16_777_215;
+export const Int24 = Schema.Number.pipe(
+	Schema.between(0, INT24_MAX, {
+		identifier: "Int24",
+		description: "a 24-bit (3 byte) signed integer",
 	}),
 );
 
@@ -28,16 +43,24 @@ export const Uint8 = Schema.Number.pipe(
 	}),
 );
 
+const INT16_MAX = 65_535;
 export const Uint16 = Schema.Number.pipe(
-	Schema.between(0, 65_535, {
+	Schema.between(0, INT16_MAX, {
 		identifier: "Uint16",
 		description: "a 16-bit unsigned integer",
 	}),
 );
 
+const INT32_MAX = 2_147_483_648;
 export const Int32 = Schema.Number.pipe(
-	Schema.between(-2_147_483_648, 2_147_483_647, {
+	Schema.between(-INT32_MAX, INT32_MAX, {
 		identifier: "Int32",
 		description: "a 32-bit signed integer",
 	}),
 );
+
+export function safeParseInt(str: string, radix: number = 10): Effect.Effect<Option.Option<number>> {
+	return Effect.gen(function* () {
+		return yield* Effect.try(() => Number.parseInt(str, radix)).pipe(Effect.option);
+	});
+}
