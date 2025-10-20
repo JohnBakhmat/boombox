14f36d73f6de6939239c9e4c6cd8472394992981
John Bakhmat <johnbakhmat@gmail.com>
2025-10-20 16:50:05 +0300

    add posthog

--- web/src/components/providers.tsx
+++ web/src/components/providers.tsx
@@ -2,10 +2,23 @@
 
 import { Provider as JotaiProvider } from "jotai";
 import { ReactNode } from "react";
+import { PostHogProvider } from "posthog-js/react";
 
 type Props = {
 	children: ReactNode;
 };
 export function Providers({ children }: Props) {
-	return <JotaiProvider>{children}</JotaiProvider>;
+	return (
+		<PostHogProvider
+			apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+			options={{
+				api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+				defaults: '2025-05-24',
+				capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+				debug: import.meta.env.MODE === "development",
+			}}
+		>
+			<JotaiProvider>{children}</JotaiProvider>
+		</PostHogProvider>
+	);
 }
